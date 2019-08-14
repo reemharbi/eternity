23554b75f0e6ab4f6d3810f8954fc46e67993f8a
Hazim Alblowi <hazim.alblowi@gmail.com>
2019-08-14 13:03:29 +0300

    Add img and tags to the ProjectItem component

--- eternity/src/components/projects/ProjectItem.js
+++ eternity/src/components/projects/ProjectItem.js
@@ -1,17 +1,23 @@
 import React, { Component } from 'react'
 import './ProjectItem.css';
+import PosterImage from './PosterImage';
+import ProjectTag from './ProjectTag';
 
 export default class ProjectItem extends Component {
     render() {
+        const tagsList = this.props.project.tags.map((tag, index) => {
+            return <ProjectTag tag={tag}/>
+        })
         return (
             <div className="project" >
                <h2>{this.props.project.name}</h2>
+               <PosterImage img={this.props.project.img_url} />
                 <h4> By: {this.props.project.by}</h4>
                 <h5>Project NO: {this.props.project.no}</h5>
                 <p>{this.props.project.text}</p>
                 <p>Git Link: {this.props.project.git_url}</p>
                 <p>Deployed Link:{this.props.project.deployed_url}</p>
-
+                <p>{tagsList}</p>
             </div>
         )
     }
