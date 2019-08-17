210fdc66ef8624fbdd391457ec4b8959f76c8010
reemharbi <reemharbi.3@gmail.com>
2019-08-17 23:58:27 +0300

    Added Semantic-ui and Semantic-ui-react

--- eternity/src/Home.js
+++ eternity/src/Home.js
@@ -1,12 +1,35 @@
 import React, { Component } from 'react'
-import logo from './images/Triskelion_B.png'
+import logo from './images/Triskelion_A.png'
+import { Image, Divider, Menu, Segment } from 'semantic-ui-react'
+
+
 export default class Home extends Component {
+    state = { activeItem: 'home' }
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
     render() {
+        const { activeItem } = this.state
         return (
-            <div>
-                <img id="logo" class="animated inifinite rotateIn" src={logo} />
-                <h1>Hello</h1>
-            </div>
+
+            <Segment inverted>
+        <Menu inverted pointing secondary>
+        <Image src={logo} size='mini' />
+    <Divider hidden />
+          <Menu.Item name='home' active={activeItem === 'home'} onClick={this.handleItemClick} />
+          <Menu.Item
+            name='messages'
+            active={activeItem === 'messages'}
+            onClick={this.handleItemClick}
+          />
+          <Menu.Item
+            name='friends'
+            active={activeItem === 'friends'}
+            onClick={this.handleItemClick}
+          />
+        </Menu>
+      </Segment>
+            
         )
     }
 }
