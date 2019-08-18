d89aab4ce35d93ba9585a58f8ee0d866521cf670
Hazim Alblowi <hazim.alblowi@gmail.com>
2019-08-18 10:33:30 +0300

    change projects routes style

--- eternity/src/components/projects/ProjectItem.js
+++ eternity/src/components/projects/ProjectItem.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import './ProjectItem.css';
 import PosterImage from './PosterImage';
 import ProjectTag from './ProjectTag';
+import { Card, Icon, Image, CardContent } from 'semantic-ui-react'
 
 export default class ProjectItem extends Component {
     render() {
@@ -9,16 +10,39 @@ export default class ProjectItem extends Component {
             return <ProjectTag tag={tag}/>
         })
         return (
-            <div className="project" >
-               <h2>{this.props.project.name}</h2>
-               <PosterImage img={this.props.project.img_url} />
-                <h4> By: {this.props.project.by}</h4>
-                <h5>Project NO: {this.props.project.no}</h5>
-                <p>{this.props.project.text}</p>
-                <p>Git Link: {this.props.project.git_url}</p>
-                <p>Deployed Link:{this.props.project.deployed_url}</p>
-                <p>{tagsList}</p>
-            </div>
+            <Card>
+            <Image src={this.props.project.img_url}  wrapped ui={false} />
+
+
+    <Card.Content>
+      <Card.Header>{this.props.project.name}</Card.Header>
+      <Card.Meta>
+        <span className='name'> By: {this.props.project.by}</span>
+        <span className='name'> Project NO: {this.props.project.no}</span>
+        
+      </Card.Meta>
+      <Card.Description>
+         {this.props.project.text}
+      </Card.Description>
+
+    </Card.Content>
+    <Card.Content extra>
+          <a href={this.props.project.git_url} target="_blank" >
+          <Icon name="github square" size="big"/>
+          </a>
+
+          <a href={this.props.project.deployed_url} target="_blank" >
+          <Icon name="play" size="big"/>
+          </a>
+          
+      </Card.Content>
+
+      <Card.Content extra>
+
+      <p>{tagsList}</p>
+          
+      </Card.Content>
+            </Card>
         )
     }
 }
