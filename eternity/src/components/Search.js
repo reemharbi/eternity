3e1b0a3285a932a41c9239ef1ced0d741f54f194
Hazim Alblowi <hazim.alblowi@gmail.com>
2019-08-19 15:14:06 +0300

    Add pagination to projects

--- eternity/src/components/Search.js
+++ eternity/src/components/Search.js
@@ -11,7 +11,7 @@ export default class Search extends Component {
                  type="search"
                        name="search"
                        value={this.props.value}
-                       onChange={this.props.onChange}/>
+                       onChange={(v)=>this.props.onChange(v)}/>
             </div>
         )
     }
--- eternity/src/components/projects/Projects.js
+++ eternity/src/components/projects/Projects.js
@@ -1,20 +1,53 @@
 import React, { Component } from 'react';
 import ProjectsList from './projectsList';
 import Search from '../Search';
+
 import {
+    Pagination,
     Grid,
     Divider,
     Container
   } from 'semantic-ui-react'
 export default class Projects extends Component {
-    render() {
+    state = {
+        activePage: 1,
+        boundaryRange: 1,
+        siblingRange: 1,
+      }
 
+    onChange = (v) => {
+        this.setState({
+            activePage: 1
+        })
+        this.props.onChange(v)
+
+    }
+
+    handlePaginationChange = (e, { activePage }) => {
+          console.log(activePage);
+          this.setState({ activePage })}
+
+    render() {
+        const {
+            activePage,
+            boundaryRange,
+            siblingRange,
+          } = this.state
         return (
+   
 
             <Container textAlign='center'>     
-                <Search onChange={this.props.onChange} value={this.props.searchValue}/>
+                <Search onChange={(v)=>this.onChange(v)} value={this.props.searchValue}/>
                 <Divider horizontal>Projects</Divider>
-                <ProjectsList projects={this.props.projects} />
+                <Pagination
+                    activePage={activePage}
+                    boundaryRange={boundaryRange}
+                    onPageChange={this.handlePaginationChange}
+                    siblingRange={siblingRange}
+                    totalPages={Math.ceil(this.props.projects.length/6)}
+
+          />
+                <ProjectsList projects={this.props.projects} activePage={this.state.activePage}/>
             </Container>      
         )
     }
