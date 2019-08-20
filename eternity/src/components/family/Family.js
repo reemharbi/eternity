37229be88f71e2befefe67639821e969c506bd61
Hazim Alblowi <hazim.alblowi@gmail.com>
2019-08-20 14:33:23 +0300

    Structure family route components

--- eternity/src/components/family/Family.js
+++ eternity/src/components/family/Family.js
@@ -1,11 +1,18 @@
-import React, { Component } from 'react'
-
+import React, { Component } from 'react';
+import {
+    Divider,
+    Container
+  } from 'semantic-ui-react';
+import InstructorsList from './InstructorsList';
 export default class Family extends Component {
     render() {
         return (
-            <div>
-                <h1>Family Here</h1>
-            </div>
+            <Container textAlign='center'>     
+
+            <Divider horizontal>The Family</Divider>
+            <InstructorsList instructors={this.props.instructors}/>
+
+            </Container>
         )
     }
 }
