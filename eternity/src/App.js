b8c990fcdccc05e317f5160178ece56b6b0f0403
Hazim Alblowi <hazim.alblowi@gmail.com>
2019-08-21 09:46:44 +0300

    Search projects by projects NO

--- eternity/src/App.js
+++ eternity/src/App.js
@@ -109,7 +109,8 @@ export default class App extends Component {
       const filteredProjects = prevState.projects.filter( project => {
         return (
           project.name.toLowerCase().includes(newSearchValue.toLowerCase()) ||
-          project.by[0].toLowerCase().includes(newSearchValue.toLowerCase())
+          project.by[0].toLowerCase().includes(newSearchValue.toLowerCase()) ||
+          project.no == newSearchValue
         )
       })
 
