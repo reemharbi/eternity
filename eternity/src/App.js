1ca78f486415d8706e93d58f57190fa82f8aeb52
Hazim Alblowi <hazim.alblowi@gmail.com>
2019-08-24 18:47:47 +0300

    Refactor the code, Moved usman easter egg to sperate component

--- eternity/src/App.js
+++ eternity/src/App.js
@@ -16,6 +16,7 @@ import aaaLogo from './images/aaa_logo.png';
 import pikminLogo from './images/pikmin_logo.png';
 import saraCatsLogo from './images/sara_cats.jpg';
 import axios from 'axios';
+import UsmanCLI from './components/UsmanCLI.js';
 import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 import ModelAzzam from './components/ModelAzzam';
 import {
@@ -73,30 +74,13 @@ export default class App extends Component {
 			displayedProjects: [],
 			instructors: [],
 			students: [],
+
 			visibleLogo: logo,
 			azzam: false,
 			websiteAlive: true,
 
 			user: null,
-			userInfo: null,
-
-			secretText: '',
-			savedText: 'sudo  rm  -rf  /',
-			pos: 0,
-			secretText1: '',
-			savedText1: 'rm: /sei/projects: projects has been deleted',
-
-			secretText2: '',
-			savedText2: 'rm: /pikmin/red: RIP Hazim',
-
-			secretText3: '',
-			savedText3: 'rm: /pikmin/yellow: RIP Reem',
-
-			secretText4: '',
-			savedText4: 'rm: /pikmin/blue: RIP Bedour',
-
-			secretText5: '',
-			savedText5: 'rm: /pikmin: Goodbye Pikmin'
+			userInfo: null
 		};
 
 		this.login = this.login.bind(this);
@@ -217,46 +201,6 @@ export default class App extends Component {
 				websiteAlive: false
 			};
 		});
-		setTimeout(this.changeText.bind(this), 1000);
-	};
-	text1 = () => {
-		this.setState((prevState, props) => {
-			return {
-				secretText1: prevState.savedText1
-			};
-		});
-		setTimeout(this.text2.bind(this), 2000);
-	};
-	text2 = () => {
-		this.setState((prevState, props) => {
-			return {
-				secretText2: prevState.savedText2
-			};
-		});
-		setTimeout(this.text3.bind(this), 2000);
-	};
-	text3 = () => {
-		this.setState((prevState, props) => {
-			return {
-				secretText3: prevState.savedText3
-			};
-		});
-		setTimeout(this.text4.bind(this), 2000);
-	};
-	text4 = () => {
-		this.setState((prevState, props) => {
-			return {
-				secretText4: prevState.savedText4
-			};
-		});
-		setTimeout(this.text5.bind(this), 2000);
-	};
-	text5 = () => {
-		this.setState((prevState, props) => {
-			return {
-				secretText5: prevState.savedText5
-			};
-		});
 	};
 
 	componentDidMount() {
@@ -308,19 +252,6 @@ export default class App extends Component {
 		});
 	}
 
-	changeText() {
-		this.setState((prevState, props) => {
-			return {
-				secretText: prevState.secretText + prevState.savedText.charAt(prevState.pos),
-				pos: prevState.pos + 1
-			};
-		});
-		if (this.state.pos < this.state.savedText.length) {
-			setTimeout(this.changeText.bind(this), 200);
-		} else {
-			setTimeout(this.text1.bind(this), 2000);
-		}
-	}
 	render() {
 		const { menuFixed, activeItem } = this.state;
 
@@ -401,7 +332,7 @@ export default class App extends Component {
 											</List>
 										</Popup>
 									) : (
-										<Label primary onClick={this.login} size="large" as='a' color='blue'>
+										<Label primary onClick={this.login} size="large" as="a" color="blue">
 											Sign in
 										</Label>
 									)}
@@ -452,40 +383,7 @@ export default class App extends Component {
 			);
 		} else {
 			return (
-				<Container>
-					<style>
-						{`
-      html, body {
-        background-color: #000000 !important;
-      }
-      p {
-        color: white;
-        font-size: 60px:
-        font-family: "Consolas"
-      }
-      .green {
-        color: green;
-      }
-      .blue {
-        color: blue;
-      }
-      span {
-
-      }
-    }
-    `}
-					</style>
-
-					<p>
-						<span className="green">ThePikmin@PC-A4JAE23</span>:<span className="blue">/sei/projects/eternity</span>
-					</p>
-					<p>$ {this.state.secretText}</p>
-					<p>{this.state.secretText1}</p>
-					<p>{this.state.secretText2}</p>
-					<p>{this.state.secretText3}</p>
-					<p>{this.state.secretText4}</p>
-					<p>{this.state.secretText5}</p>
-				</Container>
+				<UsmanCLI/>
 			);
 		}
 	}
